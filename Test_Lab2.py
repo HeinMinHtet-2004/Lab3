121ad4b7fe3fab11a17116d20c6aadce81e16373
HeinMinHtet-2004 <HEINMIN.23@ichat.sp.edu.sg>
2024-10-28 09:29:03 +0800

    Submodule with test functions

---
+++ Test_Lab2.py
@@ -0,0 +1,16 @@
+import Lab2.bmi as bmi
+
+def test_bmi_normal_weight():
+    result = bmi.calcualate_bmi(57, 1.73)
+    assert result == 0
+
+def test_bmi_over_weight():
+    result = bmi.calcualate_bmi(85, 1.73)
+    assert result == 1
+
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(40, 1.73)
+    assert result == -1
+
+print("All test passed!")
+
